7207e600d21bcf230f329eff3bfbdfce0a997b09
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-03-23 14:01:52 -0700

    add initial questions for user

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {   
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a brief description of your project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installation",
+        default: "npm i"
+    },
+
+
+
+
+
+
+
+
+
+
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
